3661fa22c8abd28dbd5b21add9f25f2b7a0d9855
BlackCCCat <360252749zzx@gmail.com>
2023-09-08 11:17:36 +0800

    Update community-completion.lua

--- plugins/community-completion.lua
+++ plugins/community-completion.lua
@@ -1,21 +1,21 @@
 return {
-  { import = "astrocommunity.completion.copilot-lua" },
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 150,
-        keymap = {
-          accept = "<C-;>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
+{ import = "astrocommunity.completion.copilot-lua" },
+{
+ "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      enabled = false,
+      auto_trigger = false,
+      debounce = 150,
+      keymap = {
+        accept = "<C-;>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
       },
     },
   },
+},
 }
